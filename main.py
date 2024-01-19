49b679b25f693826d8fd2c353615a4ed55b64b18
Serena <serena.chan@ft.com>
2024-01-19 15:58:33 +0000

    add scraped data code

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 import httpx
 from prefect import flow, task
-
-
-@task(retries=2)
-def get_repo_info(repo_owner: str, repo_name: str):
-    """Get info about a repo - will retry twice after failing"""
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}"
-    api_response = httpx.get(url)
-    api_response.raise_for_status()
-    repo_info = api_response.json()
-    return repo_info
-
+from bs4 import BeautifulSoup
+import requests
+import json
 
 @task
-def get_contributors(repo_info: dict):
-    """Get contributors for a repo"""
-    contributors_url = repo_info["contributors_url"]
-    response = httpx.get(contributors_url)
-    response.raise_for_status()
-    contributors = response.json()
-    return contributors
+def get_urls():
+    """Get info about a repo - will retry twice after failing"""
+    url = "https://news.ycombinator.com/ask"
+    page = requests.get(url)
+    html = BeautifulSoup(page.content, "html.parser")
+    
+    parentElements = html.find_all('span', class_='titleline')
+    urls = []
+    for el in parentElements:
+        url_append = el.find('a').get('href')
+        title = el.find('a').text
+        new_url = 'https://news.ycombinator.com/' + url_append
+        urls.append({'url': new_url, 'title': title})
+    
+    return urls
 
+@task
+def get_comments(urls):
+    """_summary_
+        Scrape the body of the question and all the comments
+    """
+    final_obj = []
+    for obj in urls:
+        page = requests.get(obj.get('url'))
+        html = BeautifulSoup(page.content, "html.parser")
+        body = html.find('div', class_='toptext')
+        if body is not None:
+            body = body.text
+        else:
+            body = ''
+        comments = html.find_all('div', class_='comment')
+        comments_txt = [comment.text for comment in comments]
+        final_obj.append({
+            'url': obj.get('url'),
+            'title': obj.get('title'),
+            'body': body,
+            'comments': comments_txt
+        })
+    
+    print(final_obj[2])
 
+    
+    
 @flow(log_prints=True)
-def repo_info(repo_owner: str = "PrefectHQ", repo_name: str = "prefect"):
+def scraping():
     """
-    Given a GitHub repository, logs the number of stargazers
-    and contributors for that repo.
+    Scrape https://news.ycombinator.com/ask every 24 hours 
     """
-    repo_info = get_repo_info(repo_owner, repo_name)
-    print(f"Stars 🌠 : {repo_info['stargazers_count']}")
-
-    contributors = get_contributors(repo_info)
-    print(f"Number of contributors 👷: {len(contributors)}")
-
+    urls_list = get_urls()
+    comments = get_comments(urls_list)
+    
 
 if __name__ == "__main__":
-    repo_info()
+    scraping()
